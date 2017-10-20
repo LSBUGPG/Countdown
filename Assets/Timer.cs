12eb574357e654a43fc0aceba44ebb94cba65c07
Paul Sinnett <paul.sinnett@gmail.com>
2017-10-20 12:32:06 +0100

    countdown

---
+++ Assets/Timer.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Timer : MonoBehaviour
+{
+    RectTransform rectangle;
+    float time = 0.0f;
+
+    void Start()
+    {
+        rectangle = GetComponent<RectTransform>();
+        rectangle.localRotation = Quaternion.identity;
+        StartTimer();
+    }
+
+    void Update()
+    {
+        rectangle.localRotation = Quaternion.Euler(0.0f, 0.0f, -time * 6.0f);
+    }
+
+    public void StartTimer()
+    {
+        time = 0.0f;
+        StartCoroutine(TimerUpdate());
+    }
+
+    IEnumerator TimerUpdate()
+    {
+        while (time < 30.0f)
+        {
+            time = Mathf.Min(time + Time.deltaTime, 30.0f);
+            yield return null;
+        }
+    }
+}
